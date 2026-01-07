27d7e372515c410ea35b00596f4136b1c8b89dc3
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-07 18:53:47 +0530

    add navigate

--- src/Components/VendorList.jsx
+++ src/Components/VendorList.jsx
@@ -1,15 +1,19 @@
-
+import { useNavigate } from 'react-router-dom';
 //import dummmy data
 import { tableHeadings } from '../data/Vendor'
 
 // import  Component
 import VendorTableRow from './VendorTableRow'
 
+//lucid icons
+import { Plus } from 'lucide-react';
+
 function VendorList() {
+    const navigate = useNavigate();
     return (
         <>
             {/* Main Div / outer Divs */}
-            <div className='w-screen h-screen bg-gradient-to-t from-red-500  to-fuchsia-600  '>
+            <div className='w-screen h-screen bg-gradient-to-t from-red-500  to-fuchsia-600 relative '>
                 <div className='h-full flex justify-center items-center  '>
 
                     <table className="w-4/5 rounded-lg overflow-hidden text-white  border-white font-bold">
@@ -29,6 +33,7 @@ function VendorList() {
                         </tbody>
                     </table>
                 </div>
+                <button className='absolute right-8 bottom-10' onClick={()=>navigate(`/addvendor`)}><Plus color='white' size={40}/></button>
             </div>
         </>
     )
