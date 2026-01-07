b77dd7a66433e860cf356812cd7af5e1805b20b3
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-07 12:17:36 +0530

    add row

--- src/Components/VendorTableRow.jsx
+++ src/Components/VendorTableRow.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+
+//import dummmy data
+import { mockVendors } from '../data/Vendor'
+
 
 function VendorTableRow() {
-  return (
-    <div>VendorTableRow</div>
-  )
+    return (
+        <>
+            {
+                mockVendors.map((mockVendor, i) => {
+                    const { id, name, serviceType, contactPerson, email, status, rating } = mockVendor;
+
+                    return (
+                        <tr className='border border-gray-500 text-center' key={id}>
+                            <td className='p-5'>{name}</td>
+                            <td>{serviceType}</td>
+                            <td>{contactPerson}</td>
+                            <td>{email}</td>
+                            <td className={
+                                status === "Active" ? "bg-green-100 text-green-700" : "bg-gray-200 text-gray-700"
+                            }>{status}</td>
+                            <td >{rating}</td>
+
+                        </tr>
+                    )
+                })
+            }
+        </>
+    )
 }
 
 export default VendorTableRow
