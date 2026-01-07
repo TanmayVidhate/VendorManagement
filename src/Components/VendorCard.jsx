776a00242291a69b0ab64ee93b32296f402d2ece
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-07 09:26:57 +0530

    add components

---
+++ src/Components/VendorCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VendorCard() {
+  return (
+    <div>VendorCard</div>
+  )
+}
+
+export default VendorCard
---
+++ src/Components/VendorTableRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VendorTableRow() {
+  return (
+    <div>VendorTableRow</div>
+  )
+}
+
+export default VendorTableRow
