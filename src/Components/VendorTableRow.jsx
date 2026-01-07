82d2e289e6853a4f8280118188ff0962453d70e5
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-07 14:35:24 +0530

    add navigate

--- src/Components/VendorTableRow.jsx
+++ src/Components/VendorTableRow.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 
 //import dummmy data
 import { mockVendors } from '../data/Vendor'
@@ -6,6 +7,8 @@ import { mockVendors } from '../data/Vendor'
 import { Eye } from 'lucide-react';
 
 function VendorTableRow() {
+
+    const navigate = useNavigate();
     return (
         <>
             {
@@ -21,9 +24,8 @@ function VendorTableRow() {
                             <td >
                                 <span className={`px-7 py-3 rounded-full text-justify  ${status === "Active" ? "bg-green-100 text-green-700" : "bg-gray-200 text-gray-700"}`} >{status}</span>
                             </td>
-
                             <td >{rating}</td>
-                            <td ><Eye /></td>
+                            <td > <Eye onClick={()=>{navigate(`/vendorcard/${id}`)}}/> </td>
                         </tr>
                     )
                 })
