7e20b388f10386802dd9bfb1c0df97d65c88a4a1
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-07 10:11:20 +0530

    show data in table

--- src/Components/VendorList.jsx
+++ src/Components/VendorList.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+
+//import dummmy data
+import { mockVendors } from '../data/Vendor'
 
 function VendorList() {
-  return (
-    <div>VendorList</div>
-  )
+    return (
+        <>
+            {/* Main Div / outer Divs */}
+            <div>
+                <div>
+
+                    <table border="1" cellPadding="10" cellSpacing="0">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Service Type</th>
+                                <th>Contact Person</th>
+                                <th>Email</th>
+                                <th>Status</th>
+                                <th>Rating</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                mockVendors.map((mockVendor, i) => {
+                                    const { id, name, serviceType, contactPerson, email, status, rating } = mockVendor;
+
+                                    return (
+                                        <tr key={id}>
+                                            <td>{name}</td>
+                                            <td>{serviceType}</td>
+                                            <td>{contactPerson}</td>
+                                            <td>{email}</td>
+                                            <td>{status}</td>
+                                            <td className=''>{rating}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+
+
+                    </table>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default VendorList
