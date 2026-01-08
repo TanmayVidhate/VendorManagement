b08a6acf8405f2fe6c34cf33dde835a322a7237e
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-08 11:49:29 +0530

    add icon and navigate

--- src/Views/AddNewVendor.jsx
+++ src/Views/AddNewVendor.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { mockVendors } from "../data/Vendor"
+
+import { useNavigate } from 'react-router-dom';
 
 //import component
 import Form from '../Components/Form'
+
+//Lucid import
+import { House } from 'lucide-react';
+
 function AddNewVendor() {
+    const navigate = useNavigate();
     return (
         <>
-            <div className='w-screen h-[100vh] overflow-hidden overflow-y-auto  bg-gradient-to-t from-red-500  to-fuchsia-600 relative  '>
-                <Form/>
+            <div className='  w-screen h-[100vh] overflow-hidden overflow-y-auto  bg-gradient-to-t from-red-500  to-fuchsia-600 relative  '>
+                <Form />
+                
+                <div className='absolute right-8 bottom-10 '>
+                    <House color='white' onClick={()=>{navigate("/")}} size={40} />
+                </div>
             </div>
         </>
     )
