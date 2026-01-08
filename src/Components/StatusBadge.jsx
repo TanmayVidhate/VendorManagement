ab7f0fad1d47924eb3bc2d3846db2f0c9d94fa49
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-08 11:58:51 +0530

    remove import

--- src/Components/StatusBadge.jsx
+++ src/Components/StatusBadge.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-import { mockVendors } from '../data/Vendor'
-
 function StatusBadge({ status }) {
-  
+
   return (
     <span className={`px-7 py-3 rounded-full text-justify  ${status === "Active" ? "bg-green-200 text-green-700" : "bg-gray-300 text-gray-500"}`} >{status}</span> 
   )
