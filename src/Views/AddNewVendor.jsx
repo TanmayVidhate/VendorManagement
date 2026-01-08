a15e97ec2ca5472e3feeecb1b22f84ebbbf3a5c8
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-08 10:16:08 +0530

    add view

--- src/Views/AddNewVendor.jsx
+++ src/Views/AddNewVendor.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+//import component
+import Form from '../Components/Form'
 function AddNewVendor() {
-  return (
-    <>
-    
-    </>
-  )
+    return (
+        <>
+            <div className='w-screen h-[100vh] overflow-hidden overflow-y-auto  bg-gradient-to-t from-red-500  to-fuchsia-600 relative  '>
+                <Form/>
+            </div>
+        </>
+    )
 }
 
 export default AddNewVendor
