322f541cd6492c60241b816af8ca80e44fe907e3
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-09 09:25:16 +0530

    change text color

--- src/Components/Label.jsx
+++ src/Components/Label.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Label({ title }) {
   
   return (
-    <div className='w-full  text-white font-semibold text-lg md:text-xl lg:text-base '>
+    <div className='w-full  text-black font-semibold text-lg md:text-xl lg:text-base '>
       {title}  
     </div>
   )
